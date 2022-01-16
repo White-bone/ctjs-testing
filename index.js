94547c4c508287fc7b31c0d94bd556a4548fb99f
Whitebone333 <47801148+Whitebone333@users.noreply.github.com>
2022-01-16 09:32:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-var wKeyBind = getKeyBindFromKey(Keyboard.KEY_Z, "My Z Key");
-
-function getKeyBindFromKey(key, description) {
-  var mcKeyBind = Client.getKeyBindFromKey(key);
-
-  if (mcKeyBind == null || mcKeyBind == undefined) {
-    mcKeyBind = new KeyBind(description, key);
-  }
-
-  return mcKeyBind;
-}
-if (keyboard.KEY_Z) 
-ChatLib.command("warp dungeon_hub"); 
+register("chat", (event) => {
+cancel(event);
+}).setChatCriteria("<&cYou are sending commands too fast! Please slow down.&r>"); 
